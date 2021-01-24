4ce600c02f4d3b4bb729e999a5596ec5d0fcb51d
laurentiubig <laurentiubig@gmail.com>
2021-01-24 18:48:32 +0200

    updating app with shop change, collection preview and item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./menu-item.styles.scss";
 import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
-  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
