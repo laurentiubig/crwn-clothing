fd6407880f9abd903e136fe356a5776fc30850f2
laurentiubig <laurentiubig@gmail.com>
2021-01-31 22:02:09 +0200

    updated shop data with new data, updated carts utils to group items in cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,11 @@
 //root reducer= base reducer that represents all of the state of the app and the code that combines all of our states together
 
 import { combineReducers } from 'redux';
+import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
+
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
