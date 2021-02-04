4269f93937b15590991da0f02ff06e5ce1a015bb
laurentiubig <laurentiubig@gmail.com>
2021-02-04 15:34:44 +0200

    added and basic config for redux persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,20 @@
 //root reducer= base reducer that represents all of the state of the app and the code that combines all of our states together
 
-import { combineReducers } from 'redux';
-import cartReducer from './cart/cart.reducer';
-import userReducer from './user/user.reducer';
+import { combineReducers } from "redux";
+import cartReducer from "./cart/cart.reducer";
+import userReducer from "./user/user.reducer";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
 
+const persisConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
 });
+
+export default persistReducer(persisConfig, rootReducer);
