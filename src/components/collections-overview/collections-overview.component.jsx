69310f09904b3495f1818f11f7d453b61abc08ac
laurentiubig <laurentiubig@gmail.com>
2021-02-12 18:43:18 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponging comp for directory/shop

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collections-overview.styles.scss";
-import { selectCollections } from "../../redux/shop/shop.selector.js";
+import { selectCollections, selectCollectionsForPreview } from "../../redux/shop/shop.selector.js";
 import { createStructuredSelector } from "reselect";
 
 
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
+    collections: selectCollectionsForPreview
   });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selector";
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2>CATEGORY PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
